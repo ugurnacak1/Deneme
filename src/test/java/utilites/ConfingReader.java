14d39921b672e174010837914765605cdd9986f2
ugur bey <ufuktandogan06@hotmail.com>
2022-04-05 14:23:17 +0300

    1.0

---
+++ src/test/java/utilites/ConfingReader.java
@@ -0,0 +1,36 @@
+package utilites;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfingReader {
+    public static Properties properties;
+
+    static {
+
+        String path="configuration.properties";
+
+        try {
+
+            FileInputStream fis=new FileInputStream(path);
+            properties=new Properties();
+            properties.load(fis);
+            fis.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static String getProperty(String key){
+
+        return properties.getProperty(key);
+    }
+
+
+
+}
---
+++ src/test/resources/features/US_006medunnasearch.feature
@@ -0,0 +1,2 @@
+Feature: US_006medunnasearch.featrue
+  Scenario: TS01 Medunna anasayfa Giriş Testi
